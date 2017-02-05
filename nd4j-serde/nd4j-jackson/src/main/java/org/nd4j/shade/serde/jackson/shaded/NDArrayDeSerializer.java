5a14d743254e7b01a4a0fa74962e3cf1451b96d1
François Garillot <francois@skymind.io>
2017-02-05 15:41:21 +0100

    DIAGNOSTIC: do not merge
    Investigating NdArraySerializerTest/testSerde failure.

--- nd4j-serde/nd4j-jackson/src/main/java/org/nd4j/shade/serde/jackson/shaded/NDArrayDeSerializer.java
+++ nd4j-serde/nd4j-jackson/src/main/java/org/nd4j/shade/serde/jackson/shaded/NDArrayDeSerializer.java
@@ -7,6 +7,7 @@ import org.nd4j.shade.jackson.core.JsonParser;
 import org.nd4j.shade.jackson.databind.DeserializationContext;
 import org.nd4j.shade.jackson.databind.JsonDeserializer;
 import org.nd4j.shade.jackson.databind.JsonNode;
+import org.nd4j.shade.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -19,6 +20,9 @@ public class NDArrayDeSerializer extends JsonDeserializer<INDArray> {
     @Override
     public INDArray deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
         JsonNode node = jp.getCodec().readTree(jp);
+        ObjectMapper mapper = new ObjectMapper();
+        String pretty = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(node);
+        System.out.println("NODE: " + pretty);
         String field = node.get("array").asText();
         INDArray ret = Nd4jBase64.fromBase64(field);
         return ret;
