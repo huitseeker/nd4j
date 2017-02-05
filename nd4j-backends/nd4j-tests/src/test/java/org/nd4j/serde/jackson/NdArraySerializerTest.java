f12e05c37ca0eb472570f09e1120f3052751c663
François Garillot <francois@skymind.io>
2017-02-05 15:41:21 +0100

    DIAGNOSTIC: do not merge
    Investigating NdArraySerializerTest/testSerde failure.

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/serde/jackson/NdArraySerializerTest.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/serde/jackson/NdArraySerializerTest.java
@@ -29,6 +29,7 @@ public class NdArraySerializerTest {
     public void testSerde() throws Exception {
         String json = objectMapper.writeValueAsString(Nd4j.create(2,2));
         INDArray assertion = Nd4j.create(2,2);
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(Nd4j.create(2,2)));
         INDArray test = objectMapper.readValue(json,INDArray.class);
         assertEquals(assertion,test);
     }
